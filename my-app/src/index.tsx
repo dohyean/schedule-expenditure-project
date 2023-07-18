441cecfa18cb16aff348a4a84a7ca0ff5f3a176c
LSJ123-123 <113840393+LSJ123-123@users.noreply.github.com>
2023-07-18 11:57:25 +0900

    test
    test

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,4 +4,6 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<React.StrictMode>
+  "hello"
+</React.StrictMode>);
