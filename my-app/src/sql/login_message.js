c21c0e4f8f06c502fdd49fc90c93afbea73a0c04
dohyean <65577625+dohyean@users.noreply.github.com>
2023-07-26 10:16:57 +0900

    2023/07/26 merge 전 최신화
    merge하기 전 최신화

--- my-app/src/sql/login_message.js
+++ my-app/src/sql/login_message.js
@@ -1,5 +1,6 @@
 var Open_dbms = require("./open_dbms.js");
 var Close_dbms = require("./close_dbms.js");
+var Sql_select = require("./sql_select.js");
 
 // 로그인 모듈
 exports.Login_message = function (id, pw) {
@@ -8,14 +9,7 @@ exports.Login_message = function (id, pw) {
 
   var db = Open_dbms.open_dbms();
 
-  var sqlQuery = "select * from user_data";
-  db.all(sqlQuery, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result);
-    }
-  });
+  Sql_select.sql_select(db, "user_data");
 
   Close_dbms.close_dbms(db);
 };
